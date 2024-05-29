c50f3641f3424c57fc9d48df64f854855ab932bc
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-05-29 11:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let name_box = document.getElementById("fname");
+
+name_box.addEventListener("change", capital);
+
+function capital(e) {
+	let str = e.target.value;
+
+	e.target.value = str.toUpperCase();
+}
